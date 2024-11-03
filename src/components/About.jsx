66af88a6ff48b45318b2f986bc480ba8be2a8ba7
setu <setulalchowhan@gmail.com>
2024-11-04 01:15:35 +0600

    button fixed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -58,7 +58,7 @@ const Details = () => {
             </p>
             <div className="flex justify-start items-center gap-5">
               <Link
-                className=" md:px-4 px-2 py-3 bg-[#FEBF00] font-[700] text-[18px] rounded-sm"
+                className=" md:px-4 px-2 py-3 bg-[#FEBF00] font-[500] md:font-[700] md:text-[18px] rounded-sm"
                 to="#"
               >
                 About More
